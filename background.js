90b5c39f47f06f17c6bfdbd4f38f3fa804815bab
jasonhshu <jasonshu@sas.upenn.edu>
2019-09-07 22:53:17 -0400

    configuring background page not working

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
-    chrome.tabs.sendMessage("test");
+    chrome.tabs.sendMessage(tab[0].id, {action: "open_dialog_box"});
     });
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
     chrome.notifications.create(
@@ -7,7 +7,8 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
         type: 'basic', 
         iconUrl: 'iconfinder_code_322435.png', 
         title: "This is a notification", 
-        message: "hello there!" 
+        message: "hello there!",
+        silent: true
         },
 
     function() {} 
